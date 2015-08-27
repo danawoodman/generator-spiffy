3a4f1512fefb96fde1581b80fde5d4b9af8f62cb
Dana Woodman <dana@danawoodman.com>
2015-08-27 16:29:09 -0700

    Initial commit.

---
+++ generators/app/index.js
@@ -0,0 +1,95 @@
+var yeoman = require('yeoman-generator')
+var chalk = require('chalk')
+var camelCase = require('camelcase')
+var yosay = require('yosay')
+
+var NODE_MODULE_CHOICE = 'NPM module'
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../../package.json')
+  },
+
+  prompting: function () {
+    var done = this.async()
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Howdy! Welcome to the badass ' + chalk.red('Spiffy') + ' generator!'
+    ))
+
+    var prompts = [
+      {
+        type: 'list',
+        name: 'projectType',
+        message: 'What kind of project is this?',
+        choices: [
+          NODE_MODULE_CHOICE,
+          'Web application (React, WebPack, etc...)',
+        ],
+        default: 0,
+      },
+      {
+        name: 'appname',
+        message: 'What\'s the name of the project?',
+        default: 'spiffy-app'
+        // TODO: Slugify name...
+      },
+      {
+        name: 'description',
+        message: 'What is your project description?',
+      },
+      {
+        name: 'username',
+        message: 'What is your Github username (or organization)?',
+      },
+      {
+        name: 'name',
+        message: 'What is your name or company?',
+      },
+      {
+        name: 'karma',
+        message: 'Do you you need Karma?',
+        default: false,
+      },
+    ]
+
+    this.prompt(prompts, function (props) {
+      this.props = props
+
+      this.props.isNpmModule = props.projectType === NODE_MODULE_CHOICE
+      this.props.camelAppName = camelCase(props.appname)
+
+      done()
+    }.bind(this))
+  },
+
+  writing: {
+    base: function () {
+      this.template('package.json', 'package.json')
+      this.template('license', 'license')
+      this.template('readme.md', 'readme.md')
+      this.copy('.editorconfig', '.editorconfig')
+      this.copy('.eslintrc', '.eslintrc')
+      this.copy('.eslintignore', '.eslintignore')
+      this.copy('.nvmrc', '.nvmrc')
+      this.copy('.gitignore', '.gitignore')
+      this.copy('.travis.yml', '.travis.yml')
+      this.copy('.nvmrc', '.nvmrc')
+    },
+
+    tests: function () {
+      if (this.props.karma) {
+        console.log('setup karma...')
+      } else {
+        console.log('setup mocha...')
+      }
+    },
+  },
+
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.options['skip-install']
+    })
+  }
+})
---
+++ generators/app/templates/readme.md
@@ -0,0 +1,38 @@
+# <%= props.appname %>
+
+![](https://img.shields.io/travis/<%= props.name %>/<%= props.appname %>.svg)
+[![Requirements Status](https://requires.io/github/<%= props.name %>/<%= props.appname %>/requirements.svg?branch=master)](https://requires.io/github/<%= props.name %>/<%= props.appname %>/requirements/?branch=master)
+<% if (props.isNpmModule) { %>![](https://img.shields.io/npm/dm/<%= props.appname %>.svg)
+![](https://img.shields.io/npm/v/<%= props.appname %>.svg)<% } %>
+
+> <%= props.description %>
+
+
+## Install
+
+```
+npm install --save <%= props.appname %>
+```
+
+
+## Usage
+
+```js
+var <%= props.camelAppName %> = require('<%= props.appname %>');
+
+<%= props.camelAppName %>('unicorns');
+//=> 'unicorns & rainbows'
+```
+
+
+## API
+
+See [the API docs](api.md) for full documentation.
+
+
+## License
+
+[MIT](license) &copy; [<%= props.name %>][author]
+
+
+[author]: https://github.com/<%= props.name %>/<%= props.appname %>
