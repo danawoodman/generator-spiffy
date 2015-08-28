a1fdfec436632cbd79067d72209cb8408ee0d63c
Dana Woodman <dana@danawoodman.com>
2015-08-27 18:34:03 -0700

    Fix tests.

--- test/test-app.js
+++ test/test-app.js
@@ -6,22 +6,37 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('spiffy:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt({
-        someOption: true
-      })
-      .on('end', done);
-  });
+  context('NPM module', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .inDir(path.join(os.tmpdir(), './temp-test'))
+        .withOptions({ 'skip-install': true })
+        .withPrompt({
+          appname: 'test-app',
+          description: 'Some description...',
+          name: 'John Smith',
+          username: 'jsmith',
+          projectType: 'NPM module',
+        })
+        .on('end', done);
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
-  });
+    it('creates files', function () {
+      assert.file([
+        'package.json',
+        '.editorconfig',
+        '.gitignore',
+        '.travis.yml',
+        'readme.md',
+        'license',
+        '.nvmrc',
+        '.eslintrc',
+        '.eslintignore',
+        'test/mocha.opts',
+        'test/helper.js',
+        'test/index-test.js',
+        'src/index.js',
+      ]);
+    });
+  })
 });
